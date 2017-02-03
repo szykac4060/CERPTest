19cb93ad129c5d743a36a8483fdeb58172cb8e83
szykac4060 <szymon.kacperski@gmail.com>
2017-02-03 16:27:37 +0100

    Update AppTest.java

--- CERP/src/test/java/tests/CERP/AppTest.java
+++ CERP/src/test/java/tests/CERP/AppTest.java
@@ -3,16 +3,6 @@ package tests.CERP;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
