88a4cb1023879e00baf21690b5ec3fadbd1c4f02
szykac4060 <szymon.kacperski@gmail.com>
2017-02-03 16:29:54 +0100

    Update AppTest.java

--- CERP/src/test/java/tests/CERP/AppTest.java
+++ CERP/src/test/java/tests/CERP/AppTest.java
@@ -4,14 +4,14 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
     public static Test suite()
-    {
+    {asdfasdf
+        asdfasdf
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+   sdfsdf
+   
+    public voasdfasdfid testApp()
     {
         assertTrue( true );
     }
